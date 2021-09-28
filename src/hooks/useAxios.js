4b21307b6257972f2a5b3e55f06c176c66e91326
Adrianna <adriannam@workmail.com>
2021-09-28 12:44:12 -0400

    react cards custom hooks

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useAxios(keyInLS, baseUrl) {
+  const [responses, setResponses] = useLocalStorage(keyInLS);
+
+  const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses(data => [...data, formatter(response.data)]);
+  };
+
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses];
+}
+
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    let value
+    try {
+      value = JSON.parse(
+        window.localStorage.getItem(key) || JSON.stringify(defaultValue)
+      )
+    } catch (e) {
+      console.log(e)
+      value = defaultValue;
+    }
+    return value;
+  })
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
+
+  return [state, setState];
+}
+
+export default useAxios
